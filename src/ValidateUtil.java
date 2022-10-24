c56db44809595c94358ddb2c0c2a77d9f1d1866a
rus <ruslan.cheremisin@gmail.com>
2022-10-24 18:33:30 +0200

    initial commit

---
+++ src/ValidateUtil.java
@@ -0,0 +1,17 @@
+public class ValidateUtil {
+    public static String validateString(String value) {
+        if (value!=null && !value.isEmpty() && !value.isBlank()) {
+            return value;
+        }else{
+            return "Incorrect entry";
+        }
+    }
+
+    public static int validateInt(int value) {
+        if (value > 0) {
+            return value;
+        } else {
+            return 0;
+        }
+    }
+}
