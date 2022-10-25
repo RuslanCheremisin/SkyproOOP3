1e44e5b486aaca234677ea042ebcee5256c93ce2
rus <ruslan.cheremisin@gmail.com>
2022-10-26 01:01:05 +0200

    ООП3 дз 3

--- src/SkyproOOP3.java
+++ src/SkyproOOP3.java
@@ -78,21 +78,5 @@ public class SkyproOOP3 {
 //    павлин, пингвин, додо
 //    чайка, альбатрос, сокол
 
-    public static class ValidateUtil {
-        public static String validateString(String value) {
-            if (value!=null && !value.isEmpty() && !value.isBlank()) {
-                return value;
-            }else{
-                return "Incorrect entry";
-            }
-        }
 
-        public static int validateInt(int value) {
-            if (value > 0) {
-                return value;
-            } else {
-                return 0;
-            }
-        }
-    }
 }
